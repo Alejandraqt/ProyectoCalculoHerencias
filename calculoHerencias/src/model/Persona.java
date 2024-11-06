5b62cf1eb2831e1943322d9accffea917dab72e2
Alejandra Durán <alejandradurannq@gmail.com>
2024-11-06 14:11:41 -0500

    clase persona creada

--- calculoHerencias/src/model/Persona.java
+++ calculoHerencias/src/model/Persona.java
@@ -2,4 +2,27 @@ package model;
 
 public class Persona {
 
+    private String nombre;
+    private double porcentaje;
+
+    public Persona(String nombre, double porcentaje) {
+        this.nombre = nombre;
+        this.porcentaje = porcentaje;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(double porcentaje) {
+        this.porcentaje = porcentaje;
+    }
 }
